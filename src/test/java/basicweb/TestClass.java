ee1e1e7adbaf4e81f5f99d44e46bd1b4ef9fb9b1
letanloc-tlooc <loclemap@gmail.com>
2024-08-02 00:15:22 -0700

    up

--- src/test/java/basicweb/TestClass.java
+++ src/test/java/basicweb/TestClass.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 
 public class TestClass {
 
